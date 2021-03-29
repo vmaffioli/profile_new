7333b164d816621d013b5f705682768499cc9f02
vmaffioli <vinimaffioli@gmail.com>
2021-03-29 15:58:05 -0300

    add BS HTML template

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
