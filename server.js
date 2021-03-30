ce363d4bcbcc5e34291a9a507b44ef5e90eb7747
vmaffioli <vinimaffioli@gmail.com>
2021-03-30 18:43:24 -0300

    added new template, lincense in /public && git push

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, './view')));
+app.use(express.static(path.join(__dirname, './client/build/')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/view/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+
+
 });
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
