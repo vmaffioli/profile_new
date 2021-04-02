beb6d17d3c1b9f94dd6874f316fc4013d9d4ccbb
vmaffioli <vinimaffioli@gmail.com>
2021-04-02 20:41:38 -0300

    fix

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, './client/build/')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
