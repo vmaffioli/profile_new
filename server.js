3a507836dae38307174821cab2f82a8528c5854b
vmaffioli <vinimaffioli@gmail.com>
2021-04-02 20:34:35 -0300

    adjusting ssl api

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, './client/build/')));
 
 
 app.get('/.well-known/pki-validation/', (req, res) => {
-  res.sendFile(path.join(__dirname, './SSL/B1D20171F33F47D8D150818A4473ABC6.txt'));
+  res.sendFile(path.join(__dirname, './.SSL/B1D20171F33F47D8D150818A4473ABC6.txt'));
 });
 
 
