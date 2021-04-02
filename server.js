feb1098ca34d0f86df6119de91d383275b9e8ac4
vmaffioli <vinimaffioli@gmail.com>
2021-04-02 20:37:24 -0300

    bug fix heroku

--- server.js
+++ server.js
@@ -7,10 +7,5 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, './client/build/')));
 
 
-app.get('/.well-known/pki-validation/', (req, res) => {
-  res.sendFile(path.join(__dirname, './.SSL/B1D20171F33F47D8D150818A4473ABC6.txt'));
-});
-
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
