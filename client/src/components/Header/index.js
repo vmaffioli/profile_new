fe80afb4ae1d4a0ec65a1338f5b8189be8137dcc
unknown <vmaffioli>
2021-06-10 13:22:22 -0300

    update navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,7 +12,7 @@ export default function Header(props) {
                             <div className="row rownav">
                                 <a href="#home" className="nav-item nav-link"><b>{props.data.nav_item001}</b></a>
                                 <a href="#about" className="nav-item nav-link"><b>{props.data.nav_item002}</b></a>
-                                <a href="#portfolio" className="nav-item nav-link"><b>{props.data.nav_item003}</b></a>
+                                {/*<a href="#portfolio" className="nav-item nav-link"><b>{props.data.nav_item003}</b></a>*/}
                                 <a href="#experience" className="nav-item nav-link"><b>{props.data.nav_item004}</b></a>
                                 {/*<a href="#blog" className="nav-item nav-link"><b>{props.data.nav_item005}</b></a>*/}
                                 <a href="#footer" className="nav-item nav-link"><b>{props.data.nav_item006}</b></a>
